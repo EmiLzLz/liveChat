0895977895797fa9978accac80c799151d9dbbcb
emiliano <contact@emilianoll.com>
2023-08-07 19:43:54 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express  from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+
+const app = express();
+// servidor http
+const server = http.createServer(app);
+// servidor socket
+const io = new SocketServer(server);
+
+// when client makes a connection
+io.on("connection", socket => {
+    console.log(socket.id);
+
+    // Comes from Frontend
+    socket.on("message", (body) => {
+        // Send message and author from backend
+        socket.broadcast.emit("message", {
+            body,
+            from: socket.id.slice(6),
+        })
+    })
+})
+
+server.listen(4000);
+console.log("Server on port: ", 4000)
